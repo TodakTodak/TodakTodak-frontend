80ab574d4aa97ade81f3194a36a85b076526a21b
takhyunKim <kimkih1218@gmail.com>
2021-05-08 21:19:56 +0900

    Add: pulic detail component & best post logic

--- screens/CounselingCenter/CounselingCenter.js
+++ screens/CounselingCenter/CounselingCenter.js
@@ -3,8 +3,10 @@ import {
   ImageBackground,
   ScrollView,
   StyleSheet,
-  View
+  View,
+  Text
 } from "react-native";
+import { useSelector } from "react-redux";
 
 import Title from "../../components/Title/Title";
 import CategoryButton from "../../components/CategoryButton/CategoryButton";
@@ -14,10 +16,12 @@ import { getCategoryPosts } from "../../api/postApi";
 
 import backgroundImage from "../../assets/pngs/background.png";
 
-function CounselingCenter() {
+function CounselingCenter({ navigation }) {
   const [posts, setPosts] = useState([]);
+  const [bestPost, setBestPost] = useState(null);
   const [postCategory, setpostCategory] = useState("취업");
   const [errorMessage, setErrorMessage] = useState("");
+  const currentUser = useSelector((state) => state.userReducer);
 
   const categorys = [
     { title: "취업", color: "rgba(255, 56, 56, 0.3)" },
@@ -30,7 +34,11 @@ function CounselingCenter() {
   useEffect(() => {
     (async function getCategorys() {
       try {
-        const { categoryPosts, errorMessage } = await getCategoryPosts(postCategory);
+        const {
+          errorMessage,
+          categoryPosts,
+          highestLikesPost
+        } = await getCategoryPosts(postCategory);
 
         if (errorMessage) {
           setErrorMessage(errorMessage);
@@ -38,6 +46,7 @@ function CounselingCenter() {
         }
 
         setPosts(categoryPosts);
+        setBestPost(highestLikesPost);
       } catch (err) {
         console.log("에러발생");
 
@@ -54,9 +63,9 @@ function CounselingCenter() {
         focusValue={postCategory}
         handleClick={setpostCategory}
         categoryColor={category.color}
-        categoryContainerStyle={styles.catagoryContainer}
         categoryStyle={styles.categoryStyle}
         bottomBarStyle={styles.bottomBarStyle}
+        categoryContainerStyle={styles.catagoryContainer}
       />
     );
   };
@@ -67,10 +76,21 @@ function CounselingCenter() {
         _id,
         title,
         comments,
+        contents,
         createdAt,
         isAnonymous,
         ownerNickname,
       } = post;
+
+      const handlePostClick = () => {
+        navigation.navigate("DetailPost", {
+          contents,
+          category: postCategory,
+          userId: currentUser.email,
+          inputStyle: styles.postStyle,
+          postOwner: isAnonymous ? "익명" : ownerNickname,
+        });
+      };
 
       return (
         <CategoryPostCard
@@ -79,9 +99,20 @@ function CounselingCenter() {
           comments={comments}
           createdAt={createdAt}
           isAnonymous={isAnonymous}
+          handleClick={handlePostClick}
           ownerNickname={ownerNickname}
         />
       );
+    });
+  };
+
+  const handleBestPostClick = () => {
+    navigation.navigate("DetailPost", {
+      category: postCategory,
+      userId: currentUser.email,
+      contents: bestPost.contents,
+      inputStyle: styles.postStyle,
+      postOwner: bestPost.isAnonymous ? "익명" : bestPost.ownerNickname
     });
   };
 
@@ -91,13 +122,33 @@ function CounselingCenter() {
       style={styles.backgroundContainer}
     >
       <View style={styles.container}>
-        <Title text="고민 상담소"
+        <Title
+          text="고민 상담소"
           textStyle={styles.title}
           imageStyle={styles.titleImage}
         />
-        <ScrollView style={styles.categoryWrapper} horizontal={true}>
+        <ScrollView
+          style={styles.categoryWrapper}
+          horizontal={true}
+        >
           {renderCategorys()}
         </ScrollView>
+        <View style={styles.bestPost}>
+          <Text style={styles.bestTitle}>
+            {postCategory} 토닥토닥을 많이 받은 고민
+          </Text>
+          {bestPost &&
+            <CategoryPostCard
+              title={bestPost.title}
+              comments={bestPost.comments}
+              createdAt={bestPost.createdAt}
+              cardStyle={styles.bestPostCard}
+              handleClick={handleBestPostClick}
+              isAnonymous={bestPost.isAnonymous}
+              ownerNickname={bestPost.ownerNickname}
+            />
+          }
+        </View>
         <ScrollView styles={styles.postsWrapper}>
           {renderCategoryPosts()}
         </ScrollView>
@@ -149,6 +200,23 @@ const styles = StyleSheet.create({
     marginRight: "auto",
     marginBottom: 0,
     marginLeft: "auto"
+  },
+  bestPostCard: {
+    marginBottom: 30
+  },
+  bestPost: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#ffffff",
+    borderBottomWidth: 3,
+  },
+  bestTitle: {
+    color: "rgb(235, 255, 0)",
+    fontWeight: "bold"
+  },
+  postStyle: {
+    height: 200
   }
 });
 
