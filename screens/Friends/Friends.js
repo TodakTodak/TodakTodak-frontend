0b3f061cb11b45527a3a901e299047bb6a01e564
takhyunKim <kimkih1218@gmail.com>
2021-05-12 13:58:41 +0900

    Add: FrinedCard component & route waitingFriend, friend view

--- screens/Friends/Friends.js
+++ screens/Friends/Friends.js
@@ -2,19 +2,18 @@ import React, { useEffect, useState } from "react";
 import {
   StyleSheet,
   View,
-  Text,
-  ImageBackground
+  ImageBackground,
+  ScrollView
 } from "react-native";
 import { useSelector } from "react-redux";
 
 import Title from "../../components/Title/Title";
-import Button from "../../components/Button/Button";
+import FriendCard from "../../components/FriendCard/FriendCard";
 import CategoryButton from "../../components/CategoryButton/CategoryButton";
 
 import {
   getFriends,
-  getWaitingFriends,
-  patchPendingFriend
+  getWaitingFriends
 } from "../../api/userApi";
 
 import backgroundImage from "../../assets/pngs/background.png";
@@ -65,89 +64,13 @@ function Friends() {
     }
   }, [activeCategory]);
 
-  const handlePendingButtonClick = async (friendEmail) => {
-    const friendInfo = {
-      friendEmail,
-      user: user.email
-    };
-
-    try {
-      const response = await patchPendingFriend(friendInfo);
-
-      console.log(response);
-    } catch (err) {
-      console.log(err.message, "에러 터짐");
-
-      setErrorMessage("요청에 실패했습니다");
-    }
-  };
-
   const renderFriends = () => {
-    return friendList.map((friend) => {
-      let friendStatus = "친구";
-
-      if (friend.status) {
-        switch (friend.status) {
-          case "SendPending":
-            friendStatus = "대기 중"
-            break;
-
-          case "ReceivePending":
-            friendStatus = "수락 여부 체크"
-            break;
-
-          case "SendReject":
-            friendStatus = "거부 했음"
-            break;
-
-          case "ReceiveReject":
-            friendStatus = "거부 당함"
-            break;
-
-          default:
-            break;
-        }
-      }
-
-      return (
-        <View
-          key={friend.nickname ? friend.nickname : friend.userId.nickname}
-          style={styles.friend}
-        >
-          <View style={styles.frinedInfo}>
-            <Text>
-              {friend.userId ? friend.userId.nickname : friend.nickname}
-            </Text>
-            <Text>{friendStatus}</Text>
-          </View>
-          {friendStatus === "수락 여부 체크" &&
-            <View style={styles.buttons}>
-              <Button
-                text="수락"
-                buttonStyle={styles.firendButton}
-                textStyle={styles.buttonText}
-                handleClick={() => handlePendingButtonClick(friend.userId.email)}
-              />
-              <Button
-                text="거절"
-                buttonStyle={styles.firendButton}
-                textStyle={styles.buttonText}
-              />
-            </View>
-          }
-          {friendStatus === "친구" &&
-            <View style={styles.buttons}>
-              <Button
-                text="채팅하기"
-                buttonStyle={styles.firendButton}
-                textStyle={styles.buttonText}
-              />
-            </View>
-          }
-        </View>
-      );
-    });
-  };
+    return friendList.map((friend, index) =>
+      <FriendCard
+        key={index}
+        friendInfo={friend} user={user}
+      />
+    )};
 
   return (
     <ImageBackground
@@ -169,9 +92,9 @@ function Friends() {
             categoryStyle={styles.waitingFriendCategory}
           />
         </View>
-        <View styles={styles.friendsContainer}>
+        <ScrollView styles={styles.friendsContainer}>
           {renderFriends()}
-        </View>
+        </ScrollView>
       </View>
     </ImageBackground>
   );
@@ -191,33 +114,6 @@ const styles = StyleSheet.create({
     height: "70%",
     justifyContent: "center",
     alignItems: "center"
-  },
-  friend: {
-    width: "90%",
-    minHeight: "10%",
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexDirection: "row",
-    margin: 20,
-    padding: 5,
-    borderWidth: 3,
-    borderRadius: 15,
-    backgroundColor: "#ffffff"
-  },
-  frinedInfo: {
-    width: "60%",
-    marginLeft: 15
-  },
-  buttons: {
-    flexDirection: "row"
-  },
-  firendButton: {
-    minWidth: "30%",
-    backgroundColor: "rgba(0, 0, 0, 0)"
-  },
-  buttonText: {
-    fontSize: 20,
-    color: "#000000"
   },
   categoryWrapper: {
     width: "100%",
